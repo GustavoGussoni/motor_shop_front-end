fa32244093511c2971ec8c32d0bb8ee1eeaabe4b
Adriano Souza <adrianos924@gmail.com>
2023-06-07 15:40:41 -0400

    feat: create coments

---
+++ src/Components/Coments/index.tsx
@@ -0,0 +1,22 @@
+import { CardComents } from '../CardComents';
+
+const user = {
+  username: 'Adriano Souza',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste, illo deserunt? Sapiente dolores voluptatibus delectus aperiam. Quibusdam dignissimos fugit ab ratione explicabo tenetur, iusto adipisci eum hic. Repellat, beatae mollitia.',
+  createdAt: new Date(),
+};
+
+
+export const Coments = () => {
+  return (
+    <section className="bg-grey-10 container w-screen h-screen rounded gap-6 py-9 px-7 flex flex-col max-w-screen-md">
+      <h2 className="text-xl">Comentários</h2>
+      <ul className='flex flex-col gap-7'>
+        <CardComents user={user} />
+        <CardComents user={user} />
+        <CardComents user={user} />
+      </ul>
+    </section>
+  );
+};
