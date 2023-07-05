de069f525f4ed71876dcd419b576a8223842ec57
gusdinizmaia <gustavodiniz37@hotmail.com>
2023-07-05 15:49:46 -0300

    fix: alteração feita, agora o fuel está funcionando de maneira dinâmica

--- src/Components/Form/FormRegisterAnnouncement/annoucement.schema.ts
+++ src/Components/Form/FormRegisterAnnouncement/annoucement.schema.ts
@@ -1,10 +1,21 @@
 import * as z from "zod";
+import { FuelTypes } from "./@types";
+
+export const typeFuels: FuelTypes = {
+  1: "flex",
+  2: "eletrico",
+  3: "hibrido",
+};
 
 export const AnnouncementSchema = z.object({
   brand: z.string().max(20, "Insira uma marca válida").nonempty(),
   model: z.string().max(127).nonempty("O modelo é obrigatório"),
   year: z.string().length(4).nonempty("O ano é obrigatório"),
-  fuel: z.number().max(3).min(1, "O combustível é obrigatório"),
+  fuel: z
+    .number()
+    .max(3)
+    .min(1, "O combustível é obrigatório")
+    .transform((fuel: number) => typeFuels[fuel]),
   kilometers: z.number().min(1, "A quilometragem é obrigatório").max(999999),
   color: z.string().max(20).nonempty("O cor é obrigatória"),
   price_fipe: z.number().min(1, "O preço da tabela FIPE é obrigatória"),
