a95bfbfe84138c5585dfad6c8aabe869a46c8bfb
gusdinizmaia <gustavodiniz37@hotmail.com>
2023-07-06 14:04:08 -0300

    feat: adicionado o viewImage

--- src/Components/ModalDefault/index.tsx
+++ src/Components/ModalDefault/index.tsx
@@ -1,16 +1,20 @@
 import { Dialog } from "@headlessui/react";
 
 interface iModalDefault {
-    children: React.ReactNode;
-    open: boolean;
-    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  children: React.ReactNode;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const ModalDefault = ({ children, open, setOpen }: iModalDefault) => {
-    return (
-        <Dialog className='absolute z-50' open={open} onClose={() => setOpen(false)}>
-            <div className='fixed inset-0 bg-black/30' aria-hidden='true' />
-            <div className='fixed inset-0 overflow-y-auto'>{children}</div>
-        </Dialog>
-    );
+  return (
+    <Dialog
+      className="absolute z-50"
+      open={open}
+      onClose={() => setOpen(false)}
+    >
+      <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+      <div className="fixed inset-0 overflow-y-auto">{children}</div>
+    </Dialog>
+  );
 };
